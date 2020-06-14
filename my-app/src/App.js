dcc919c19338f18d5db16bbac3a0286526a89ce5
Anna Ågren <info@annaagren.se>
2020-06-14 22:34:27 +0200

    om button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import skier from "./images/skier.svg";
 import logo from "./images/logo.svg";
+
+import { About } from "./about";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <About />
       <header className="App-header">
         <img src={skier} className="skier" alt="skier" />
         <img src={logo} className="logo" alt="logo" />
---
+++ my-app/src/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState } from "react";
+
+import "./about.css";
+
+export const About = () => {
+  const [open, setOpen] = useState(false);
+
+  return <div className="om-button">OM</div>;
+};
