85d43ca82198f951c78030e8820fd81759c56865
Anna Ågren <info@annaagren.se>
2020-06-15 22:10:17 +0200

    border box etc

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,21 +17,29 @@ function App() {
       <header className="App-header">
         <img src={skier} className="skier" alt="skier" />
         <img src={logo} className="logo" alt="logo" />
-        <section>
-          <p>YES! VI ÅKER RULLSKIDOR PÅ NYLAGD ASFALT I NATURSKÖNA ORHEM.</p>
-          <div className="spacing" />
-          <p>SÖNDAGAR KL 9.00.</p>
-          <p>
-            VI SAMLAS{" "}
-            <span>
-              <a href="https://goo.gl/maps/wveUkTtHc3opYTwV9" target="_blank">
-                HÄR
-              </a>
-            </span>
-            .
-          </p>
-        </section>
       </header>
+      <section className="main-section">
+        <p>
+          YES! VI ÅKER RULLSKIDOR PÅ <br />
+          NYLAGD ASFALT I NATURSKÖNA ORHEM.
+        </p>
+        <div className="spacing" />
+        <p>SÖNDAGAR KL 9.00.</p>
+        <p>
+          VI SAMLAS{" "}
+          <span>
+            <a href="https://goo.gl/maps/wveUkTtHc3opYTwV9" target="_blank">
+              HÄR
+            </a>
+          </span>
+        </p>
+      </section>
+
+      <footer className="footer">
+        <div>instagram</div>
+        <div>hello@girlsonskis.se</div>
+        <div>©Girls on Skis 2020</div>
+      </footer>
     </div>
   );
 }
--- my-app/src/about.js
+++ my-app/src/about.js
@@ -5,17 +5,16 @@ import "./about.css";
 export const AboutText = function (props) {
   return (
     <div className={"about-txt " + (props.visible ? "visible " : "")}>
-      <p className="pm ">
+      {/* <p className="pm ">
         Det monotona klickandet av stavarna mot asfalten. Fåglar som kvittrar.
         Solen som värmer. Det oändliga glidet. Inte en tanke på att det är
         jobbigt. Vilan i nerförsbacken, fartvinden svalkar...
-      </p>
+      </p> */}
       <p className="pm ">
         Girls on Skis är ett intitiativ grundat 2020 för dig som ❤️ längd – och
         vill åka rullskidor när det inte finns snö. Vi åker för att det är
-        härligt. Och sjukt bra träning.{" "}
-        <span className="highlight">Vill du åka med oss?</span>
-        <a>Let us know!</a>
+        härligt. Och sjukt bra träning. Vill du åka med oss?
+        <a>Anmäl dig här!</a>
       </p>
       <p className="pm ">
         Alla nivåer är välkomna. All åkning sker på egen risk.
