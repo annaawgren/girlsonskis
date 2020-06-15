3c1c6b8d5029566ef9ceece2f8edb6b10f157829
Anna Ågren <info@annaagren.se>
2020-06-15 18:23:02 +0200

    txt changes

--- my-app/src/about.js
+++ my-app/src/about.js
@@ -5,16 +5,23 @@ import "./about.css";
 export const AboutText = function (props) {
   return (
     <div className={"about-txt " + (props.visible ? "visible " : "")}>
-      Det monotona klickandet av stavarnas spetsar i asfalten. Fåglarnas
-      kvitter. Solen som värmer ryggen och fartvinden som svalkar. Det går lätt.
-      Tänker inte på att hela kroppen jobbar och att jag faktiskt flåsar på rätt
-      rejält. Vilan i nerförsbacken.
-      <p>
-        Girls on Skis är ett intitiativ grundat 2020 för dig som ❤️ längd. Vi
-        åker för att det är härligt! Och sjukt bra träning. Vill du åka med oss?
-        <a>Let us know!</a>
-      </p>
-      <p>Alla nivåer är välkomna. All åkning sker på egen risk.</p>
+      <div className="column">
+        <p>
+          Det monotona klickandet av stavarna mot asfalten. Fåglar som kvittrar.
+          Solen värmer. Det går lätt. Tänker inte på att hela kroppen jobbar och
+          att jag faktiskt flåsar på rätt rejält. Vilan i nerförsbacken,
+          fartvinden som svalkar.
+        </p>
+        <p className="pm">
+          Girls on Skis är ett intitiativ grundat 2020 för dig som ❤️ längd. Vi
+          åker för att det är härligt! Och sjukt bra träning. Vill du åka med
+          oss?
+          <a>Let us know!</a>
+        </p>
+        <p className="pm">
+          >Alla nivåer är välkomna. All åkning sker på egen risk.
+        </p>
+      </div>
     </div>
   );
 };
