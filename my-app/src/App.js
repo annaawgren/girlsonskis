8a8dd8b48f966269c3a587a0aa1927ca4132a943
Anna Ågren <info@annaagren.se>
2020-06-15 21:25:33 +0200

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -19,6 +19,16 @@ function App() {
         <img src={logo} className="logo" alt="logo" />
         <section>
           <p>YES! VI ÅKER RULLSKIDOR PÅ NYLAGD ASFALT I NATURSKÖNA ORHEM.</p>
+          <p>SÖNDAGAR KL 9.00.</p>
+          <p>
+            VI SAMLAS{" "}
+            <span>
+              <a href="https://goo.gl/maps/wveUkTtHc3opYTwV9" target="_blank">
+                HÄR
+              </a>
+            </span>
+            .
+          </p>
         </section>
       </header>
       <footer className="footer" />
--- my-app/src/about.js
+++ my-app/src/about.js
@@ -5,23 +5,21 @@ import "./about.css";
 export const AboutText = function (props) {
   return (
     <div className={"about-txt " + (props.visible ? "visible " : "")}>
-      <div className="column">
-        <p>
-          Det monotona klickandet av stavarna mot asfalten. Fåglar som kvittrar.
-          Solen värmer. Det går lätt. Tänker inte på att hela kroppen jobbar och
-          att jag faktiskt flåsar på rätt rejält. Vilan i nerförsbacken,
-          fartvinden som svalkar.
-        </p>
-        <p className="pm">
-          Girls on Skis är ett intitiativ grundat 2020 för dig som ❤️ längd. Vi
-          åker för att det är härligt! Och sjukt bra träning. Vill du åka med
-          oss?
-          <a>Let us know!</a>
-        </p>
-        <p className="pm">
-          >Alla nivåer är välkomna. All åkning sker på egen risk.
-        </p>
-      </div>
+      <p className="pm ">
+        Det monotona klickandet av stavarna mot asfalten. Fåglar som kvittrar.
+        Solen som värmer. Det oändliga glidet. Inte en tanke på att det är
+        jobbigt. Vilan i nerförsbacken, fartvinden svalkar...
+      </p>
+      <p className="pm ">
+        Girls on Skis är ett intitiativ grundat 2020 för dig som ❤️ längd – och
+        vill åka rullskidor när det inte finns snö. Vi åker för att det är
+        härligt. Och sjukt bra träning.{" "}
+        <span className="highlight">Vill du åka med oss?</span>
+        <a>Let us know!</a>
+      </p>
+      <p className="pm ">
+        Alla nivåer är välkomna. All åkning sker på egen risk.
+      </p>
     </div>
   );
 };
