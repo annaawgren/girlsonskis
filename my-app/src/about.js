20f2704e3f747663f7c01be981791f6eb7c3ef7f
Anna Ågren <info@annaagren.se>
2020-06-17 14:57:11 +0200

    imgs changes

--- my-app/src/about.js
+++ my-app/src/about.js
@@ -5,12 +5,12 @@ import "./about.css";
 export const AboutText = function (props) {
   return (
     <div className={"about-txt " + (props.visible ? "visible " : "")}>
-      <p className="pm ">
-        Girls on Skis är för dig som ❤️ längd – och vill åka rullskidor när det
-        inte finns snö. Vi åker för att det är härligt. Och sjukt bra träning.
-        Vill du åka med oss?
-        <a>Anmäl dig här!</a>
+      <p className="pm">
+        Girls on Skis är för dig som &#60;3 längd – och vill åka rullskidor när
+        det inte finns snö. Vi åker för att det är härligt. Och sjukt bra
+        träning. Vill du åka med oss? <a>Anmäl dig här!</a>
       </p>
+      <div className="spacing" />
       <p className="pm ">
         Alla nivåer är välkomna. All åkning sker på egen risk.
       </p>
